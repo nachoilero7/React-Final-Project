84e61882f31e137458c2092c160f05c0c26f8b15
nachoilero7 <nachoilero7@gmail.com>
2017-03-14 21:48:23 -0300

    Ahi hay una idea de MainView que seria la home

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import MainView from './mainView/mainView';
-import HomeView from './homeView/homeView';
-import AboutView from './aboutView/aboutView';
+//despues de route va el switch que intercambia las vistas
 
 const getRoutes = function() {
     return (
         <div>
             <Route name="Main" component={MainView} />
-            <Switch>
-                <Route exact path="/" component={HomeView} />
-                <Route path="/about" component={AboutView} />
-            </Switch>
+            
         </div>
     )
 };
--- src/routes/mainView/mainView.js
+++ src/routes/mainView/mainView.js
@@ -1,6 +1,7 @@
 import './mainView.css';
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+//import link lo necesitan las otras view
+//import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 
 class MainView extends Component {
@@ -9,14 +10,22 @@ class MainView extends Component {
             <article className="main-view">
                 <header className="main-view__header">
                     <img src={logo} className="main-view__logo" alt="logo" />
-                    <h2>Welcome to React</h2>
                 </header>
-                <nav className="main-view__nav">
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/about">About</Link></li>
-                    </ul>
-                </nav>
+                <header className="main-view__title">
+                    <h4>Welcome to</h4>
+                    <h1>Spotisearch</h1>
+                    <p>Search your favourite song over Spotify, just enter an artist's name in the<br />
+                    following search box and enjoy!</p>
+                </header>
+                <div className="main-view__search">
+                    <input type="text" className="main-view__search-query" placeholder="Type the name of your favorite artist..." />
+                </div>
+                <div className="main-view__favourites">
+                    <h3>Favorite Songs</h3>
+                    <div className="main-view__favourites-stored">
+
+                    </div>
+                </div>
             </article>
         );
     }
